f4b2dcab0ce629af9efcad4431c22a97a651dd85
Mike Looijmans <milo-software@users.sourceforge.net>
2010-05-08 15:00:42 +0200

    AutoBackup add crontab and wpa_supplicant.conf

--- PLi/AutoBackup/settings-backup.sh
+++ PLi/AutoBackup/settings-backup.sh
@@ -16,7 +16,7 @@ cp -a /etc/enigma2/* ${BACKUPDIR}/backup/enigma2/
 [ -d /home/root/.ssh ] && cp -rp /home/root/.ssh ${BACKUPDIR}/backup/
 cp -p /etc/dropbear/dropbear_rsa_host_key ${BACKUPDIR}/backup/ 
 
-for file in CCcam.cfg CCcam.prio CCcam.channelinfo CCcam.providers radegast.cfg newsreader.conf NETcaster.conf resolv.conf fstab passwd
+for file in CCcam.cfg CCcam.prio CCcam.channelinfo CCcam.providers radegast.cfg newsreader.conf NETcaster.conf resolv.conf fstab passwd wpa_supplicant.conf
 do
 	[ -f /etc/$file ] && cp -p /etc/$file ${BACKUPDIR}/backup/
 done
@@ -25,8 +25,10 @@ cp /etc/samba/smb.conf ${BACKUPDIR}/backup/
 
 [ -f /etc/tuxbox/config/newcamd.conf ] && cp -p /etc/tuxbox/config/newcamd.conf ${BACKUPDIR}/backup/
 
+crontab -l > /tmp/crontab 2> /dev/null && cp -f /tmp/crontab ${BACKUPDIR}/backup/crontab
+rm -f /tmp/crontab
+
 # todo: other cams?
-# todo: crontab?
 # todo: network?
 # todo: default?
 
