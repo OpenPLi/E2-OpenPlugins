7801f74c85ac9e82a1bbad1eb9b4fbc3b20645a1
Dzjaivnt <mark@vanboom.nl>
2010-12-26 22:01:24 +0100

    AutoBackup: Big change in backup
    Signed-off-by: Dzjaivnt <mark@vanboom.nl>
    Signed-off-by: MiLo <milo-software@users.sourceforge.net>

--- PLi/AutoBackup/settings-backup.sh
+++ PLi/AutoBackup/settings-backup.sh
@@ -9,46 +9,87 @@ fi
 BACKUPDIR="$1"
 PLI_BACKUPFILE=/usr/lib/enigma2/python/Plugins/PLi/AutoBackup/backup.cfg
 USER_BACKUPFILE=/etc/backup.cfg
+USER_AUTOINSTALL=/etc/autoinstall
+INSTALLED=/etc/installed
+TEMP_INSTALLED=/tmp/installed
+RESTORE_TEMP=/tmp/restore.cfg
 
-echo "Backup to ${BACKUPDIR}"
-
-mkdir -p ${BACKUPDIR}/backup/enigma2
-cp -a /etc/enigma2/* ${BACKUPDIR}/backup/enigma2/
-
-[ -d /home/root/.ssh ] && cp -rp /home/root/.ssh ${BACKUPDIR}/backup/
-cp -p /etc/dropbear/dropbear_rsa_host_key ${BACKUPDIR}/backup/
-
-if [ -f ${PLI_BACKUPFILE} ]
+if [ -x /usr/bin/opkg ]
 then
-for file in $(cat ${PLI_BACKUPFILE})
-do
-        [ -f /etc/$file ] && cp -p /etc/$file ${BACKUPDIR}/backup/
-done
+	IPKG=/usr/bin/opkg
+else
+	IPKG=ipkg
 fi
 
-if [ -f ${USER_BACKUPFILE} ]
+echo "Backup to ${BACKUPDIR}/backup/"
+
+if [ -f ${PLI_BACKUPFILE} ] 
 then
-for file in $(cat ${USER_BACKUPFILE})
-do
-        [ -f $file ] && cp -p $file ${BACKUPDIR}/backup/
-done
+	for file in $(cat ${PLI_BACKUPFILE})
+	do
+		if [ -f $file ]
+		then
+			echo $file >> ${RESTORE_TEMP}
+		else
+			if [ -d $file ]
+			then
+			echo $file >> ${RESTORE_TEMP}
+			fi
+		fi
+	done
 fi
 
-cp /etc/samba/smb.conf ${BACKUPDIR}/backup/
-
-if [ -d /etc/tuxbox/config ]
+if [ -f ${USER_BACKUPFILE} ] 
 then
-	mkdir -p ${BACKUPDIR}/backup/tuxbox
-	cp -rp /etc/tuxbox/config ${BACKUPDIR}/backup/tuxbox/
+	for file in $(cat ${USER_BACKUPFILE})
+	do
+		if [ -f $file ]
+		then
+			echo $file >> ${RESTORE_TEMP}
+		else
+			if [ -d $file ]
+			then
+			echo $file >> ${RESTORE_TEMP}
+			fi
+		fi
+	done
 fi
 
-crontab -l > /tmp/crontab 2> /dev/null && cp -f /tmp/crontab ${BACKUPDIR}/backup/crontab
-rm -f /tmp/crontab
+cp -f /etc/passwd /tmp && echo /tmp/passwd >> ${RESTORE_TEMP}
+
+if [ -f /etc/fstab ]
+then
+  for fstype in cifs nfs swap
+  do
+    grep -q " ${fstype} " /etc/fstab || grep " ${fstype} " /etc/fstab >> /tmp/fstab
+  done
+echo /tmp/fstab >> ${RESTORE_TEMP} 
+fi
+
+crontab -l > /tmp/crontab 2> /dev/null && echo /tmp/crontab >> ${RESTORE_TEMP}
+
+tar -czf ${BACKUPDIR}/backup/PLi-AutoBackup.tar.gz --files-from=/tmp/restore.cfg 2> /dev/null
+
+if [ -f ${INSTALLED} ]
+then
+	echo "Generating ${BACKUPDIR}/backup/autoinstall"
+	${IPKG} list_installed | awk '{print $1}' > ${TEMP_INSTALLED}
+	diff ${INSTALLED} ${TEMP_INSTALLED} | grep '+' | grep -v '@' | grep -v '+++' | sed 's/^+//' > ${BACKUPDIR}/backup/autoinstall
+	
+	if [ -f ${USER_AUTOINSTALL} ] 
+	then
+		cat ${USER_AUTOINSTALL} >> ${BACKUPDIR}/backup/autoinstall
+	fi
+fi
 
 # todo: other cams?
 # todo: network?
 # todo: default?
 
 touch ${BACKUPDIR}/backup/.timestamp
-
+rm -f /tmp/restore.cfg
+rm -f /tmp/crontab
+rm -f /tmp/fstab
+rm -f /tmp/passwd
+rm -f ${TEMP_INSTALLED}
 true
